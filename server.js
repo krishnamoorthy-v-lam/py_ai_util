5f34bb36acb89ceb3363dec84dec14d84a0c60af
krishnamoorthy-v-lam <krishnamoorthy@lamynaals.com>
2025-11-03 12:40:07 +0530

    cors added

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+const cors = require("cors");
+app.use(cors());
+
+
 app.use((req, res, next) => {
   console.log(req.method, "  ", req.url);
   next();
